a6ebe0e001161cb6bca6e20afc929daadc25794a
Daniel Segura <dagase10@gmail.com>
2025-10-15 22:49:38 -0600

    Commit inicial

---
+++ BD/Create.sql
@@ -0,0 +1,36 @@
+CREATE TABLE PartidosML (
+    id_partido INT PRIMARY KEY,
+
+    fecha DATE,
+    hora TIME,
+    
+    equipo_local VARCHAR(50),
+    equipo_visitante VARCHAR(50),
+    id_equipo_local INT,
+    id_equipo_visitante INT, -- Tienen que coinicidir con EquiposRef ¿Realmente mejorará el performance? No estoy sobresimplificando?
+    
+    goles_local_final INT,
+    goles_visitante_final INT,
+    resultado_final VARCHAR(1), -- H/D/A futura predicción en % 
+    goles_local_ht INT,
+    goles_visitante_ht INT, 
+    resultado_ht VARCHAR(1),
+    disparos_local INT,
+    disparos_visitante INT,
+    disparos_porteria_local INT,
+    disparos_porteria_visitante INT,
+    corners_local INT,
+    corners_visitante INT,
+    faltas_local INT,
+    faltas_visitante INT,
+    amarillas_local INT,
+    amarillas_visitante INT,
+    rojas_local INT,
+    rojas_visitante INT,
+    
+);
+
+CREATE TABLE EquiposRef (
+    id_equipo INT PRIMARY KEY,
+    nombre_equipo VARCHAR(50) UNIQUE
+);
